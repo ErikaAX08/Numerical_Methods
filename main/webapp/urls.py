efad43be8e35ecc35925a6eb34418c55251cca14
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-02-13 17:26:41 -0600

    Added logic to newton raphson page

--- main/webapp/urls.py
+++ main/webapp/urls.py
@@ -3,6 +3,7 @@ from .views.home_views import index
 from .views.taylor_series_views import calculate_taylor, taylor_series
 from .views.regula_falsi_views import calculate_falsi, regula_falsi
 from .views.bisection_method_views import calculate_bisection_method, bisection_method
+from .views.newton_raphson_method_view import newton_raphson, calculate_newton_raphson
 
 urlpatterns = [
     path("", index, name="index"),
@@ -12,4 +13,6 @@ urlpatterns = [
     path("calculate-falsi/", calculate_falsi, name="calculate_falsi"),
     path("bisection-method/", bisection_method, name="bisection_method"),
     path("calculate-bisection-method/", calculate_bisection_method, name="calculate_bisection_method"),
+    path("newton-raphson-method/", newton_raphson, name="newton_raphson_method"),
+    path("calculate-newton-raphson-method/", calculate_newton_raphson, name="calculate_newton_raphson_method"),
 ]
