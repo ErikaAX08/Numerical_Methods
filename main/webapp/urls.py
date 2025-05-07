b4ac2e9e222a8e1db916ae88c2b4e52d82da1e2f
Ivan Luna <atreus@Ivans-MacBook-Pro.local>
2025-05-06 18:22:10 -0600

    New archives for trapezoid compund rule, archives modified and url.py modified to add the method

--- main/webapp/urls.py
+++ main/webapp/urls.py
@@ -18,6 +18,7 @@ from .views.gauss_maximum_column_pivoting_views import gauss_maximum_column_pivo
     calculate_gauss_maximum_column_pivoting
 from .views.chulesky_factorization_view import chulesky_factorization, calculate_chulesky_factorization
 from .views.lu_factorization_view import lu_factorization, calculate_lu_factorization
+from .views.trapezoid_compound_views import trapezoid_compound, calculate_trapezoid_compound
 
 urlpatterns = [
     path("", index, name="index"),
@@ -52,4 +53,7 @@ urlpatterns = [
     path("lu-factorization/", lu_factorization, name="lu_factorization"),
     path("calculate-lu-factorization/", calculate_lu_factorization,
          name="calculate_lu_factorization"),
+    path("trapezoid-compound/", trapezoid_compound, name="trapezoid_compound"), 
+    path("calculate-trapezoid-compound/", calculate_trapezoid_compound,
+         name="calculate_trapezoid_compound"),
 ]
